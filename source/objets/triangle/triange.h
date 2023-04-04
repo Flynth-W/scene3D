373898c3de6d7859c84fa6ec2f07f55b87c9b48b
flynth <flynth.dev@gmail.com>
2023-04-04 10:56:26 -0400

    Fix: objet Triangle

--- source/objets/triangle/triange.h
+++ source/objets/triangle/triange.h
@@ -5,19 +5,8 @@
 // shader class
 #include "./../Shader/Shader.h"
 
-class Trinagle : public ObjetAbs {
-  public: 
-    void Init( const char * vertexShader, const char * fragShader , float * vertices , unsigned int sizeVert, unsigned int * indices , unsigned int  sizeInd)override;
-    void Update() override;
-    void Render() override;
-    void Release() override;
-    bool isRun ;
-  private:
-    ShaderUtil * shader;
-    unsigned int VBO, VAO, EBO;
-};
 
-class Trian: public Objet{
+class Triangle: public Objet{
 
   public:
     void addShader(const char * vertexShader, const char * fragShader) override ;
--- source/objets/triangle/triangle.cpp
+++ source/objets/triangle/triangle.cpp
@@ -2,11 +2,16 @@
 #include <iostream>
 using namespace std;
 
-void Trinagle::Init(const char *vertexShader, const char *fragShader, float *vertices, unsigned int sizeVert, unsigned int *indices, unsigned int sizeInd) {
-  cout << " init Triangle obj" << endl;
-  shader=new ShaderUtil();
-  shader->Load(vertexShader, fragShader );
-  
+
+//class trian
+//
+
+void Triangle::addShader(const char *vertexShader, const char *fragShader){
+  shader=new Shader();
+  shader->load(vertexShader, fragShader );
+}
+void Triangle::addBuffer(float *vertices, unsigned int sizeVert, unsigned int *indices, unsigned int sizeInd){
+
   // buffer
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -26,43 +31,14 @@ void Trinagle::Init(const char *vertexShader, const char *fragShader, float *ver
     // texture coord attribute
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+}
+void Triangle::Init(){} ;
+void Triangle::Update(){
 
 };
-void Trinagle::Update(){
+void Triangle::Renderer(){
   shader->use();
   glBindVertexArray(VAO);
   glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 };
-void Trinagle::Render(){};
-void Trinagle::Release(){};
-
-//class trian
-//
-
-void Trian::addShader(const char *vertexShader, const char *fragShader){
-  shader=new Shader();
-  shader->load(vertexShader, fragShader );
-}
-void Trian::addBuffer(float *vertices, unsigned int sizeVert, unsigned int *indices, unsigned int sizeInd){
-
-  // buffer
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeVert, vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeInd , indices, GL_STATIC_DRAW);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // texture coord attribute
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-} 
