4b66bf7e395a540161bb4bfaff22f1f5a05fcbdd
flynth <flynth.dev@gmail.com>
2023-04-04 09:36:45 -0400

    Mejore: triangle to tria

--- source/objets/triangle/triange.h
+++ source/objets/triangle/triange.h
@@ -2,6 +2,8 @@
 #include "../abstract.h"
 #include "../../utils/handleShader/headerShader.h"
 #include "../../utils/buffer/buff.h"
+// shader class
+#include "./../Shader/Shader.h"
 
 class Trinagle : public ObjetAbs {
   public: 
@@ -12,5 +14,18 @@ class Trinagle : public ObjetAbs {
     bool isRun ;
   private:
     ShaderUtil * shader;
+    unsigned int VBO, VAO, EBO;
+};
+
+class Trian: public Objet{
+
+  public:
+    void addShader(const char * vertexShader, const char * fragShader) override ;
+    void addBuffer(float * vertices , unsigned int sizeVert, unsigned int * indices , unsigned int  sizeInd) override ;
+    void Init()      override ;
+    void Update()    override ;
+    void Renderer()  override ;
+  private:
+    Shader * shader;
     unsigned int VBO, VAO, EBO;
 };
--- source/objets/triangle/triangle.cpp
+++ source/objets/triangle/triangle.cpp
@@ -36,3 +36,33 @@ void Trinagle::Update(){
 };
 void Trinagle::Render(){};
 void Trinagle::Release(){};
+
+//class trian
+//
+
+void Trian::addShader(const char *vertexShader, const char *fragShader){
+  shader=new Shader();
+  shader->load(vertexShader, fragShader );
+}
+void Trian::addBuffer(float *vertices, unsigned int sizeVert, unsigned int *indices, unsigned int sizeInd){
+
+  // buffer
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeVert, vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeInd , indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+} 
