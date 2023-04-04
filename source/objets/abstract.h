f4e956ccf3f4262cd39c61e75c6d7860123c1063
flynth <flynth.dev@gmail.com>
2023-04-04 09:25:29 -0400

    include Class abstract Objet{}

--- source/objets/abstract.h
+++ source/objets/abstract.h
@@ -4,8 +4,18 @@
 class ObjetAbs{
   public:
     virtual void Init( const char * vertexShader, const char * fragShader , float * vertices , unsigned int sizeVert, unsigned int * indices , unsigned int  sizeInd){};
+    virtual void Buffer(){};
     virtual void Update(){};
     virtual void Render(){};
     virtual void Release(){};
     bool isRun;
+};
+
+class Objet{
+  public:
+    virtual void addShader();
+    virtual void addBuffer();
+    virtual void Init();
+    virtual void Update();
+    virtual void Renderer();
 };
