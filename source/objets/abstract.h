dd389b05b69055f2eb820963e7fd439bde8acb4e
flynth <flynth.dev@gmail.com>
2023-04-04 10:58:51 -0400

    Mejore: objetAbs to Objet

--- source/objets/abstract.h
+++ source/objets/abstract.h
@@ -1,16 +1,5 @@
 #pragma once
 
-
-class ObjetAbs{
-  public:
-    virtual void Init( const char * vertexShader, const char * fragShader , float * vertices , unsigned int sizeVert, unsigned int * indices , unsigned int  sizeInd){};
-    virtual void Buffer(){};
-    virtual void Update(){};
-    virtual void Render(){};
-    virtual void Release(){};
-    bool isRun;
-};
-
 class Objet{
   public:
     virtual void addShader(const char * vertexShader, const char * fragShader){};
